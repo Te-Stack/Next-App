3e045e9f23f6e4fdefaae6869181974ba8de97fe
Quincy Oghenetejiri <ukumakubequincy@gmail.com>
2021-08-26 21:07:14 +0100

    Working

--- pages/index.js
+++ pages/index.js
@@ -6,11 +6,11 @@ import Link from "next/link"
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <h1>Home </h1>
-      <p>lorem</p>
+    <div className={styles.title}>
+      <h1 className={styles.text}>Home </h1>
+      <p className={styles.text}>lorem</p>
       <Link href="/ninjas">
-        <a>See Ninja Listing</a>
+        <a className={styles.btn}>See Ninja Listing</a>
       </Link>
     </div>
   )
