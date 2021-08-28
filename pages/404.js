b67f10e57e7387fd3e29ee9b47d47c33d88f418e
Quincy Oghenetejiri <ukumakubequincy@gmail.com>
2021-08-28 23:14:11 +0100

    Working

--- pages/404.js
+++ pages/404.js
@@ -1,6 +1,16 @@
 import Link from "next/link"
+import { useEffect } from "react"
+import {useRouter} from "next/router"
 
 const NotFound = () => {
+    const router = useRouter();
+
+    useEffect(()=>{
+        setTimeout(()=>{
+            router.push("/")
+        })
+    })
+
     return ( 
         <div className="not-found">
             <h1>Ooooops</h1>
