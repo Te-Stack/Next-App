7f78f8537e8df0450ce1849f5e2dc565b0e61a4a
Quincy Oghenetejiri <ukumakubequincy@gmail.com>
2021-08-29 20:17:38 +0100

    Working

--- components/Navbars.js
+++ components/Navbars.js
@@ -1,10 +1,13 @@
 import Link from "next/link"
+import Image from "next/image"
+
 
 const Navbar = () => {
     return ( 
         <nav>
             <div className="logo">
-                <h1>Ninja List</h1>
+                <Image src="/vercel.svg" width={128} height={77} />
+                
             </div>
             <Link href="/"><a>Home</a></Link>
             <Link href="/about"><a>About</a></Link>
