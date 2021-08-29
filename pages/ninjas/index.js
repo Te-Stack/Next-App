046ad4e6a53477c938296912c641adb213c80d41
Quincy Oghenetejiri <ukumakubequincy@gmail.com>
2021-08-29 21:04:43 +0100

    Working

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -14,7 +14,7 @@ const Ninjas = ({ ninjas }) => {
             <h1>All Ninjas</h1>
             {ninjas.map(ninja=>(
                 <div key={ninja.id}>
-                    <a className={styles.singles}>
+                    <a className={styles.single}>
                         <h3>{ninja.name} </h3>
                     </a>
 
