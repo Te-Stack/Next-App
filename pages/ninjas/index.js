56a406753a9ca82a2ce604117a5c08166eee92ed
Quincy Oghenetejiri <ukumakubequincy@gmail.com>
2021-08-31 20:56:19 +0100

    Working

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -3,6 +3,7 @@ import styles from "../../styles/Ninjas.module.css"
 export const getStaticProps = async () =>{
     const res = await fetch('https://jsonplaceholder.typicode.com/users')
     const data = res.json()
+
     return {
         props: { ninjas: data }
     }
@@ -12,14 +13,14 @@ const Ninjas = ({ ninjas }) => {
     return ( 
         <div>
             <h1>All Ninjas</h1>
-            {ninjas.map(ninja=>(
+            {ninjas.map(ninja=>{
                 <div key={ninja.id}>
                     <a className={styles.single}>
                         <h3>{ninja.name} </h3>
                     </a>
 
                 </div>
-            ))}
+            })}
        </div>
      );
 }
